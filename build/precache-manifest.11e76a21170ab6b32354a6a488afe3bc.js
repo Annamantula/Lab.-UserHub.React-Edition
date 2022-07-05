a5a1234b4c07b2731ee73989b66b1b8c85bf440e
Anna Mantula <ganna.mantula@gmail.com>
2022-07-05 15:32:25 -0500

    finished

---
+++ build/precache-manifest.11e76a21170ab6b32354a6a488afe3bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54d05ddbe038dfb0295af3100735103c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c845d81a773b460cf2d",
+    "url": "/static/css/main.1253bac1.chunk.css"
+  },
+  {
+    "revision": "0392a618e55a6705606d",
+    "url": "/static/js/2.e5615630.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.e5615630.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c845d81a773b460cf2d",
+    "url": "/static/js/main.c680dcbd.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.c680dcbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c56e041857a0bf600d3",
+    "url": "/static/js/runtime-main.3715f0a4.js"
+  }
+]);
